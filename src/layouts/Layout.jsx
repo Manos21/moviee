553e3247e3c26d3cd0caffdd3b36eee4447c4d54
Manos21 <manos.athanasiou21@gmail.com>
2024-06-09 22:18:53 +0300

    Update Layout.jsx

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,6 +3,7 @@ import { Outlet, NavLink } from "react-router-dom"
 import ReactDOM from 'react-dom/client';
 import Header from "../components/Header";
 import SearchBox from "../components/SearchBox";
+import Home from "../pages/Home";
 
 
 export default function Layout(){
