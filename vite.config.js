81ddafcd52388f9a99c4fd0dc1cab4a67be129ba
Manos21 <manos.athanasiou21@gmail.com>
2024-06-09 20:25:13 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     outDir: 'dist'
-//   }
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      },
-    },
-  },
-});
+  plugins: [react()]
+})
